f3da4861fbedd3a7271fa7d15394f933a24db31f
Egidijus <115953858+EgidijusPak@users.noreply.github.com>
2025-04-22 21:11:53 +0300

    Update animal.js

--- backend/models/animal.js
+++ backend/models/animal.js
@@ -1,7 +1,5 @@
-// backend/models/animal.js
 import pool from "../db.js";
 
-// Function to create a new animal
 export async function createAnimal({ vardas, rusis, svoris, aplinka, lt }) {
   const query = `
     INSERT INTO animals (vardas, rusis, svoris, aplinka, lt)
@@ -11,7 +9,7 @@ export async function createAnimal({ vardas, rusis, svoris, aplinka, lt }) {
 
   try {
     const [result] = await pool.query(query, values);
-    return result.insertId; // Return the ID of the new animal
+    return result.insertId; 
   } catch (error) {
     console.error("Error inserting animal:", error);
     throw new Error("Failed to insert animal into the database.");
